30ceaf473c9ac4ffb9fc89340a32cb06d7ae1e63
idideads <idideads@gmail.com>
2022-05-07 10:39:47 +0800

    67 solution

---
+++ 67_add_binary.go
@@ -0,0 +1,43 @@
+package leetcode
+
+/*
+给你两个二进制字符串，返回它们的和（用二进制表示）。
+输入为 非空 字符串且只包含数字 1 和 0。
+示例1：
+输入: a = "11", b = "1"
+输出: "100"
+示例2：
+输入: a = "1010", b = "1011"
+输出: "10101"
+*/
+func addBinary(a string, b string) string {
+	aLength, bLength := len(a), len(b)
+	var abytes, bbytes, rbytes []byte
+	longestSize := 0
+	if aLength >= bLength {
+		longestSize = aLength + 1
+	} else {
+		longestSize = bLength + 1
+	}
+	abytes, bbytes, rbytes = make([]byte, longestSize-aLength, longestSize), make([]byte, longestSize-bLength, longestSize), make([]byte, longestSize, longestSize)
+	for i, _ := range abytes {
+		abytes[i] = '0'
+	}
+	for i, _ := range bbytes {
+		bbytes[i] = '0'
+	}
+	abytes, bbytes = append(abytes, []byte(a)...), append(bbytes, []byte(b)...)
+	var c int
+	for i := longestSize - 1; i >= 0; i-- {
+		a, b := int(abytes[i])-48, int(bbytes[i])-48
+		if a+b+c > 1 {
+			rbytes[i], c = byte(a+b+c-2)+48, 1
+		} else {
+			rbytes[i], c = byte(a+b+c)+48, 0
+		}
+	}
+	if rbytes[0] == '0' {
+		return string(rbytes[1:])
+	}
+	return string(rbytes)
+}
---
+++ 67_add_binary_test.go
@@ -0,0 +1,36 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_addBinary(t *testing.T) {
+	a, b := "1111", "1111"
+	c := addBinary(a, b)
+	fmt.Println(a, b, c)
+}
+
+func Test_op(t *testing.T) {
+	s := "1"
+	b := []byte(s)
+	fmt.Println(int(b[0])-48)
+	c0, c1 := byte('0'), byte('1')
+	fmt.Printf("c0:%d\t%b\t%c\nc1:%d\t%b\t%c\n", c0, c0, c0, c1, c1, c1)
+	fmt.Println("")
+	fmt.Printf("0 & 0 = %c\t%b\n", c0&c0, c0&c0)
+	fmt.Printf("0 & 1 = %c\t%b\n", c0&c1, c0&c1)
+	fmt.Printf("1 & 1 = %c\t%b\n", c1&c1, c1&c1)
+	fmt.Println("")
+	fmt.Printf("0 | 0 = %c\t%b\n", c0|c0, c0|c0)
+	fmt.Printf("0 | 1 = %c\t%b\n", c0|c1, c0|c1)
+	fmt.Printf("1 | 1 = %c\t%b\n", c1|c1, c1|c1)
+	fmt.Println("")
+	fmt.Printf("0 ^ 0 = %c\t%b\n", c0^c0, c0^c0)
+	fmt.Printf("0 ^ 1 = %c\t%b\n", c0^c1, c0^c1)
+	fmt.Printf("1 ^ 1 = %c\t%b\n", c1^c1, c1^c1)
+	fmt.Println("")
+	fmt.Printf("0 ^ 0 | 0 = %d\t%b\n", c0^c0|'0', c0^c0|'0')
+	fmt.Printf("0 ^ 1 | 0 = %d\t%b\n", c0^c1|'0', c0^c1|'0')
+	fmt.Printf("1 ^ 1 | 0 = %d\t%b\n", c1^c1|'0', c1^c1|'0')
+}
