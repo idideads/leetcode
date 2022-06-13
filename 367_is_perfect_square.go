86521818022f6c8185349b72a2249b97bdb1d5f4
idideads <idideads@gmail.com>
2022-06-13 23:27:44 +0800

    add 367 solution

--- 367_is_perfect_square.go
+++ 367_is_perfect_square.go
@@ -1,5 +1,10 @@
 package leetcode
 
 func isPerfectSquare(num int) bool {
-	return true
+	for i := 1; i*i <= num; i++ {
+		if i*i == num {
+			return true
+		}
+	}
+	return false
 }
---
+++ 367_is_perfect_square_test.go
@@ -0,0 +1,12 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_isPerfectSquare(t *testing.T) {
+	fmt.Println(isPerfectSquare(4))
+	fmt.Println(isPerfectSquare(18))
+	fmt.Println(isPerfectSquare(19 * 19))
+}
